bb512af05115b5ef474d9ca1005afb0b654d715a
sagarkondamudi <sgrk98@gmail.com>
2018-05-17 08:37:04 -0700

    first file in git

---
+++ helloworld.c
@@ -0,0 +1,77 @@
+
+C Programming Logo
+
+    C++ Python R Tutorial Java Kotlin 
+
+    home
+    Tutorial
+    Examples
+    Library Functions
+
+search
+C "Hello, World!" Program
+A simple C program to display "Hello, World!" on the screen. Since, it's a very simple program, it is often used to illustrate the syntax of a programming language.
+C program to print
+
+To understand this example, you should have the knowledge of following C programming topics:
+
+    C Input Output (I/O)
+
+Program to Display "Hello, World!"
+
+#include <stdio.h>
+int main()
+{
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
+}
+
+Output
+
+Hello, World!
+
+How "Hello, World!" program works?
+
+    The #include <stdio.h> is a preprocessor command. This command tells compiler to include the contents of stdio.h (standard input and output) file in the program.
+    The stdio.h file contains functions such as scanf() and print() to take input and display output respectively.
+    If you use printf() function without writing #include <stdio.h>, the program will not be compiled.
+    The execution of a C program starts from the main() function.
+    The printf() is a library function to send formatted output to the screen. In this program, the printf() displays Hello, World! text on the screen.
+    The return 0; statement is the "Exit status" of the program. In simple terms, program ends with this statement.
+
+Check out these related examples:
+
+    Display its own Source Code as Output
+    Read a Line From a File and Display it
+    Display Prime Numbers Between Intervals Using Function
+    Print an Integer (Entered by the User)
+    Display Characters from A to Z Using Loop
+    Make a Simple Calculator Using switch...case
+    Display Fibonacci Sequence
+    Multiply two Matrices by Passing Matrix to a Function
+
+Related Examples
+C "Hello, World!" Program
+Print an Integer (Entered by the User)
+Add Two Integers
+Multiply two Floating Point Numbers
+Find ASCII Value of a Character
+Compute Quotient and Remainder
+Find the Size of int, float, double and char
+Receive the latest tutorial to improve your programming skills
+
+Get Latest Updates on Programiz
+
+    About Contact Advertise 
+
+Copyright © by Programiz | All rights reserved | Privacy Policy
+Related Examples
+C "Hello, World!" Program
+Print an Integer (Entered by the User)
+Add Two Integers
+Multiply two Floating Point Numbers
+Find ASCII Value of a Character
+Compute Quotient and Remainder
+Find the Size of int, float, double and char
+
